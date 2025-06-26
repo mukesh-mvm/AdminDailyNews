4286435b7a62dfa969f6b3c6530c94262103dd9a
himanshumvm <himanshusharma.mvmbs@gmail.com>
2025-06-26 11:26:41 +0530

    first commit

---
+++ src/helper/Helper.jsx
@@ -0,0 +1,7 @@
+//  const baseurl = "http://localhost:4001";
+
+// const baseurl = "https://jobpakki-backend.onrender.com";
+// const baseurl = "https://api.leadershipladder.in";
+// const baseurl = "https://api.supernpro.com";
+ const baseurl = "http://82.25.109.68:5007";
+export { baseurl };
